2ccd70b9d4570747cca6d75274e71c5aec7d25bf
Joao Teixeira <joao.teixeira.dev@gmail.com>
2024-10-17 16:36:53 -0300

    feat: driver controller

--- src/3-presentation/controller/driver/get.controller.ts
+++ src/3-presentation/controller/driver/get.controller.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata';
 import Container, { Service } from "typedi";
 import { BaseController } from "../base.controller";
 import { InputCreateDriverDto, InputGetDriverFiltersDto, OutputGetDriverDto } from '../../../2-business/dto/driver/driver.dto';
-import { defaultError } from '../../../2-business/module/errors/default';
 import { Output } from '../../../2-business/dto/output';
 import GetDriverUseCase from '../../../2-business/useCases/driver/get.useCase';
 
@@ -26,8 +25,7 @@ export class GetDriverController extends BaseController<InputCreateDriverDto, Ou
                 data: response
             }
         } catch (error) {
-            console.log(error)
-            throw defaultError
+            throw error
         }
     }
 
